dd087ca3ff231eb9894d263a8df97b2553abef9e
licheng <18892080039@163.com>
2021-04-20 22:39:55 +0800

    update

--- routes/site.js
+++ routes/site.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 
 const site = require('../controllers/site');
-
+const captcha = require('../tools/captcha');
 router.all('/*', async function(ctx, next){
 	console.log('enter site.js');
 	ctx.set('X-XSS-Protection',0)
@@ -12,5 +12,6 @@ router.all('/*', async function(ctx, next){
 router.get('/', site.index);
 router.get('/post/:id', site.post);
 router.post('/post/addComment', site.addComment);
+router.get('/captcha',captcha.captcha)
 
 module.exports = router
